7b6f485cdd554545887180d1f2e483d47fd7ec6b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-10-11 13:10:59 -0400

    Update the comments inside of the RunCampaign.

--- src/main/java/labfive/experiment/RunCampaign.java
+++ src/main/java/labfive/experiment/RunCampaign.java
@@ -7,16 +7,12 @@ import java.util.Random;
 
 public class RunCampaign implements Campaign {
 
-  /** The starting size for running an experiment campaign. */
   private static final int INPUT_SIZE_START = 250;
 
-  /** The growth factor for input when running an experiment campaign. */
   private static final int INPUT_GROWTH_FACTOR = 2;
 
-  /** The total number of input size doubles to perform for an experiment campaign. */
   private static final int CAMPAIGN_LENGTH = 10;
 
-  /** The indentation level for the output. */
   private static final String IDENTATION = "  ";
 
   public int[] generateRandomData(int size) {
